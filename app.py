88e179dffe7c54eceaae79f716081d6695b1e79f
deluxebrain <matt.caton@mac.com>
2018-12-04 08:37:54 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,6 @@
+import os
+
+from flask_restplus_demo import create_app
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
---
+++ flask_restplus_demo/__init__.py
@@ -0,0 +1,22 @@
+import os
+import logging.config
+
+from flask import Flask
+from config import config_map
+
+def create_app(config_name):
+    
+    # get the corresponding config
+    config = config_map[config_name]
+
+    # as we are using a package the application object should be given a hardcoded name
+    # the following will pull the application name assuming myapp/__init__.py
+    app = Flask(__name__.split('.')[0])
+    app.config.from_object(config)
+    config.init_app(app)
+
+    app.logger.info('>>>>> Starting Flask app using config {} <<<<<'.format(config_name))
+    return app
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
